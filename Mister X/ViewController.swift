d2fff6c8f2ca12560329f79fcd1733d8071954bf
admin <admin@dhcp-10-180-122-95.dynamic.eduroam.mwn.de>
2017-11-08 14:35:38 +0100

    Initial Commit

---
+++ Mister X/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mister X
+//
+//  Created by admin on 08.11.17.
+//  Copyright © 2017 Praktikum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
