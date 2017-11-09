9816e321ad24b1e5d06e6bd3b7aa6419fb7a5dfd
admin <admin@admins-mbp.fritz.box>
2017-11-09 23:34:07 +0100

    Alert bei neuem Spiel hinzugefügt

---
+++ Mister X/ViewControllerEinstellungen.swift
@@ -0,0 +1,48 @@
+//
+//  ViewControllerEinstellungen.swift
+//  Mister X
+//
+//  Created by admin on 09.11.17.
+//  Copyright © 2017 Praktikum. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerEinstellungen: UIViewController {
+
+    @IBAction func button_newgame(_ sender: UIButton) {
+        // create the alert
+        let alert = UIAlertController(title: "Achtung", message: "Sicher ein neues Spiel anfangen?", preferredStyle: UIAlertControllerStyle.alert)
+        
+        // add the actions (buttons)
+        alert.addAction(UIAlertAction(title: "Ja!", style: UIAlertActionStyle.destructive, handler: { action in
+            self.performSegue(withIdentifier: "newgame", sender: self)
+        }))
+        alert.addAction(UIAlertAction(title: "Abbrechen", style: UIAlertActionStyle.cancel, handler: nil))
+        
+        // show the alert
+        self.present(alert, animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
