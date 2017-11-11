04a982663e22fe5b54209b03612ef53b55574b48
admin <admin@admins-mbp.fritz.box>
2017-11-11 15:23:22 +0100

    MapKit Location tracking

--- Mister X/ViewControllerKarte.swift
+++ Mister X/ViewControllerKarte.swift
@@ -8,29 +8,81 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewControllerKarte: UIViewController {
-
+class ViewControllerKarte: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet var button_ich: UIButton!
+    @IBAction func button_ich(_ sender: UIButton) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = (locationManager.location?.coordinate)!
+        annotation.title = "Mister X"
+        annotation.subtitle = "Position von Mister X"
+        mapView.addAnnotation(annotation)
+    }
+    
+    
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
+        button_ich.layer.cornerRadius = 5
+        let blur = UIVisualEffectView(effect: UIBlurEffect(style: UIBlurEffectStyle.light))
+        blur.frame = button_ich.bounds
+        blur.isUserInteractionEnabled = false //This allows touches to forward to the button.
+        button_ich.insertSubview(blur, at: 0)
+        blur.layer.cornerRadius = 5//0.5 * button_ich.bounds.size.width
+        blur.clipsToBounds = true
+        
+        //mapView.setUserTrackingMode(.follow, animated: true)
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest//kCLLocationAccuracyNearestTenMeters
+            //locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        let location = locations.last! as CLLocation
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        self.mapView.setRegion(region, animated: true)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkLocationAuthorizationStatus()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
