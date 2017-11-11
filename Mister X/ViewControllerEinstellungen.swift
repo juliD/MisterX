93d9915a18f76847064cb747da4c13bcefa47c72
admin <admin@admins-mbp.fritz.box>
2017-11-11 22:31:00 +0100

    Einstellungen ein TableView hinzugefügt

--- Mister X/ViewControllerEinstellungen.swift
+++ Mister X/ViewControllerEinstellungen.swift
@@ -8,8 +8,7 @@
 
 import UIKit
 
-class ViewControllerEinstellungen: UIViewController {
-
+class ViewControllerEinstellungenTable: UITableViewController {
     @IBAction func button_newgame(_ sender: UIButton) {
         // create the alert
         let alert = UIAlertController(title: "Achtung", message: "Sicher ein neues Spiel anfangen?", preferredStyle: UIAlertControllerStyle.alert)
@@ -23,26 +22,31 @@ class ViewControllerEinstellungen: UIViewController {
         // show the alert
         self.present(alert, animated: true, completion: nil)
     }
+}
+
+class ViewControllerEinstellungen: UIViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
