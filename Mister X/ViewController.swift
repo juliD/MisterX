1c24156ac9a3b45785a045d0751f62f58defe084
admin <admin@admins-MBP.WLAN-Router>
2017-11-16 11:34:19 +0100

    firebase authentication und erstellen vom user beim ersten anmelden

--- Mister X/ViewController.swift
+++ Mister X/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //if userid does not exist yet sign into firebase
+        //always use locally stored uid otherwise we get problems with firebase
+        let defaults = UserDefaults.standard
+        if let uid = defaults.string(forKey: "uid"){
+            print("already logged in: "+uid)
+        }
+        else {
+            logInOnce()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /*
+     Sign into firebase anonymously, save userid to persist data
+     and create a user in firebase
+     */
+    func logInOnce(){
+        Auth.auth().signInAnonymously(completion: { (user, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            //save data locally
+            print("User logged in anonymously with uid: "+user!.uid)
+            let defaults = UserDefaults.standard
+            defaults.set(user!.uid, forKey:"uid")
+            defaults.set(0, forKey:"boost1")
+            defaults.set(0, forKey:"boost2")
+            
+            //set up firebase user
+            var ref: DatabaseReference!
+            ref = Database.database().reference()
+            
+            let boosts = [
+                "username" : "Agent",
+                "boost1" : 0,
+                "boost2" : 0
+                ] as [String : Any]
+            ref.child("user").child(user!.uid).setValue(boosts)
+        })
+
+    }
 
 }
 
